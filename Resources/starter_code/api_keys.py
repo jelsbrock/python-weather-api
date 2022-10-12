2916ad60008ac205b4ad63da3e7bbbf62a7c5085
jelsbrock <jack.elsbrock@gmail.com>
2022-10-11 23:18:35 -0500

    VacationPy.ipynb

--- Resources/starter_code/api_keys.py
+++ Resources/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "acea755b231f18391947ddb9517e4b02"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyD7swRdIBZzmkWanSlsVqvroFHAxu-zemA"
